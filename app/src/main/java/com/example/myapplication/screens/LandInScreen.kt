a8d504ba83ba60d0c9eb63f913f17960e6f48e72
Hernando Lopez <hernando.rincon@correounivalle.edu.co>
2023-10-10 22:11:32 -0500

    Se continua landing page de CineCritix, se fondo a carusel inicial de películas con titulo y genero de pelicula, descripcion de la pagina y redireccionamiento a Registro

--- app/src/main/java/com/example/myapplication/screens/LandInScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/LandInScreen.kt
@@ -5,31 +5,49 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
 import com.example.myapplication.components.CardSlider
 import com.example.myapplication.components.LoginLandInButtonComponent
 import com.example.myapplication.navigation.CineCritixAppRouter
 import com.example.myapplication.navigation.Screen
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.myapplication.components.RegisterLandInButtonComponent
 
 
 @Composable
 fun LandInScreen() {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        content = {
+            item {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -79,11 +97,83 @@ fun LandInScreen() {
                 Spacer(modifier = Modifier.height(20.dp))
 
                 CardSlider()
+
+                Text(
+                    text = "En el emocionante mundo del cine y la " +
+                            "televisión, cada experiencia es única." +
+                            "Cada película y serie es una historia " +
+                            "por descubrir, un viaje por realizar, y " +
+                            "una emoción por experimentar",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Justify
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text(
+                    text = "¿Qué esperas para registrarte?",
+                    color = colorResource(id = R.color.colorPrimary),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 22.sp,
+                    textAlign = TextAlign.Center
+                )
             }
         }
-    }
-}
 
+        Spacer(modifier = Modifier.height(20.dp))
+
+    }
+                Row(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth()
+                        .background(Color.LightGray),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Texto y botón a la izquierda
+                    Column (
+                        modifier = Modifier
+                            .weight(2f)
+                            .padding(12.dp)
+                    ){
+                        Text(
+                            text = "Únete a CineCritix " +
+                                    "y comparte tus " +
+                                    "opiniones.",
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Text(
+                            text = "Tu pasión, tus reseñas, tu comunidad.",
+                            color = Color.Gray,
+                            fontSize = 14.sp
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        RegisterLandInButtonComponent(
+                            text = "Registrate",
+                            onClick = {
+                                CineCritixAppRouter.navigateTo(Screen.RegistroScreen)
+                            }
+                        )
+                    }
+
+                    Image(
+                        painter = painterResource(id = R.drawable.moviecollage), // Reemplaza con tu imagen
+                        contentDescription = "Imagen",
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                    )
+                }
+}})}
 
 @Preview
 @Composable
