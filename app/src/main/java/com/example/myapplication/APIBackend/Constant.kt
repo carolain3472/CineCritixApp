2156b108bf7d4d5aedb94b9ae176085a86042d9b
NataliaLopezO <natalia.lopez.osorio@correounivalle.edu.co>
2023-10-29 02:02:44 -0500

    API register

--- app/src/main/java/com/example/myapplication/APIBackend/Constant.kt
+++ app/src/main/java/com/example/myapplication/APIBackend/Constant.kt
@@ -2,5 +2,5 @@ package com.example.myapplication.APIBackend
 
 object Constant {
 
-    const val BASE_URL = "https://cinecritixbackend.onrender.com"
+    const val BASE_URL = "https://cinecritixbackend.onrender.com/"
 }
--- app/src/main/java/com/example/myapplication/data/UserResponse.kt
+++ app/src/main/java/com/example/myapplication/data/UserResponse.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.data
 
 data class UserResponse(
-    var codigo: Int,
-    var mensaje: String
+    var valid: Boolean
 )
